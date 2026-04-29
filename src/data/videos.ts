9e2e50a421326fa379140203669430b8f2716f16
samlin0117 <samlin0117@gmail.com>
2026-04-29 20:40:02 +0800

    fix: update '奇妙能力歌' video ID for correct embedding

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -14,12 +14,12 @@ export interface VideoInfo {
 
 export const videoList: VideoInfo[] = [
   {
-    "id": "v-fb-410041263191028",
+    "id": "v-fb-353832705569724",
     "platform": "facebook",
     "category": "p1",
     "title": "陳粒 · 奇妙能力歌/純字幕 【cover by-Ponpon陳芃瑄】",
     "date": "2019-08-07",
-    "embedUrl": "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ponpon0405/videos/410041263191028/&show_text=0&width=500",
+    "embedUrl": "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/ponpon0405/videos/353832705569724/&show_text=0&width=500",
     "isFeatured": false
   },
   {
